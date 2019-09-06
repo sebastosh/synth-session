7dee1acc59e354dd2c38c7599cf78352fc8f4a72
sebastosh <sebastosh@gmail.com>
2019-09-05 23:42:29 -0400

    disable active storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,16 +29,16 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
-  config.action_cable.mount_path = '/cable'
-  config.action_cable.url = 'wss://localhost:3000/cable'
-  config.action_cable.allowed_request_origins = [ /http:\/\/localhost:*/ ]
+  # config.action_cable.mount_path = '/cable'
+  # config.action_cable.url = 'wss://localhost:3000/cable'
+  # config.action_cable.allowed_request_origins = [ /http:\/\/localhost:*/ ]
   
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
