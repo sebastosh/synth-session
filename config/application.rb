8893fdd7fc4b4346b47be410715bb9e0da2a58c0
sebastosh <sebastosh@gmail.com>
2019-09-06 11:49:34 -0400

    autoload paths

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module SoundAppApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.eager_load_paths << Rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('lib')
     
   end
 end
